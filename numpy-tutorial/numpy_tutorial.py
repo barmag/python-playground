682d9db7b29016b62ce4d723a388b60889ce6b8b
Yasser Makram <yasser@barm.ag>
2017-09-03 14:17:53 +0200

    basic numpy tutorial

--- numpy-tutorial/numpy_tutorial.py
+++ numpy-tutorial/numpy_tutorial.py
@@ -1,4 +1,5 @@
 # follow CS231 numpy tutorial
+import numpy as np
 
 def quicksort(arr):
     if(len(arr) <= 1):
@@ -13,3 +14,16 @@ def quicksort(arr):
 
 sorted = quicksort([3,6,76,2,4,8,4,8,32,8])
 print(sorted)
+print("*******quicksort end***********")
+
+a = np.array([1, 2, 3]) # create rank 1 array
+print(type(a), a.shape)
+b = np.array([[1,2,3],[4,5,6]])
+print(b.shape)
+
+# np array indexing
+a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+# slice first two rows and columns 1 and 2
+s = a[:2, 1:3]
+print(s)
+
