7cc05fc4074f1d4e864355aedb365572a2f2646a
Yasser Makram <yasser@barm.ag>
2017-09-16 12:48:51 +0200

    dummy is unique with sorting

--- algorithms/algorithms/isunique-chars-in-string.py
+++ algorithms/algorithms/isunique-chars-in-string.py
@@ -25,8 +25,25 @@ def dummy_is_unique(word):
         currentIndex = currentIndex + 1
     return True
 
+def dummy_is_unique_with_sorting(word):
+    '''
+    O(n log n)
+    '''
+    if(is_larger_than_charset(word)):
+        return False
+    
+    sorted_word = sorted(word)
+
+    for c in range(0, len(sorted_word)-1):
+        if sorted_word[c] == sorted_word[c+1]:
+            return False
+
+    return True
+
 assert max_char_length() == 57
 assert dummy_is_unique(c1) == False
 assert dummy_is_unique(c2) == True
 
+assert dummy_is_unique_with_sorting(c1) == False
+assert dummy_is_unique_with_sorting(c2) == True
 print("success!")
