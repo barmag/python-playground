2d3c7cea1dc4a90b2e0f25702eaeed1702af3c9e
Yasser Makram <yasser@barm.ag>
2017-11-01 08:15:55 +0200

    bug: fix forecast_move by modifying the new board after the move

--- aind-tests/minimax.py
+++ aind-tests/minimax.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 
 class GameState:
 
@@ -19,11 +20,12 @@ class GameState:
         move: tuple
             The target position for the active player's next move
         """
-        game = copy.deepcopy(self)
+        game = deepcopy(self)
         x, y = move
-        self.board[x][y] = 1
-        self.player_loc[self.parity] = move
-        self.parity ^= 1
+        game.board[x][y] = 1
+        game.player_loc[self.parity] = move
+        game.parity ^= 1
+        return game
         
     
     def get_legal_moves(self):
@@ -37,6 +39,25 @@ class GameState:
         be a pair of integers in (column, row) order specifying
         the zero-indexed coordinates on the board.
         """
-        pass
+        current_loc = self.player_loc[self.parity]
+        if not current_loc:
+            return [(x, y) for x in range(self.num_rows) for y in range(self.num_cols) if self.board[x][y] != 1 ]
 
+
+print("Creating empty game board...")
 g = GameState()
+
+print("Getting legal moves for player 1...")
+p1_empty_moves = g.get_legal_moves()
+print("Found {} legal moves.".format(len(p1_empty_moves or [])))
+
+print("Applying move (0, 0) for player 1...")
+g1 = g.forecast_move((0, 0))
+
+print("Getting legal moves for player 2...")
+p2_empty_moves = g1.get_legal_moves()
+if (0, 0) in set(p2_empty_moves):
+    print("Failed\n  Uh oh! (0, 0) was not blocked properly when " +
+          "player 1 moved there.")
+else:
+    print("Everything looks good!")
