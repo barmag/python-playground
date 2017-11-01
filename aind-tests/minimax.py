ac467ebe787ebf4334106e7ce85de4608c4cb7be
Yasser Makram <yasser@barm.ag>
2017-11-01 08:58:58 +0200

    implement terminal_test returns true if the game is over for the active player and false otherwise

--- aind-tests/minimax.py
+++ aind-tests/minimax.py
@@ -57,6 +57,32 @@ class GameState:
         return moves
 
 
+
+def terminal_test(gameState):
+    """ Return True if the game is over for the active player
+    and False otherwise.
+    """
+    legal_moves = gameState.get_legal_moves()
+    return len(legal_moves) == 0
+
+
+def min_value(gameState):
+    """ Return the value for a win (+1) if the game is over,
+    otherwise return the minimum value over all legal child
+    nodes.
+    """
+    pass
+
+
+def max_value(gameState):
+    """ Return the value for a loss (-1) if the game is over,
+    otherwise return the maximum value over all legal child
+    nodes.
+    """
+    pass
+
+
+
 print("Creating empty game board...")
 g = GameState()
 
